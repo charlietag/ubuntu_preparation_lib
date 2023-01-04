ab7690bf7cebf997a5e4bf97caf9f75e0bba0ea9
Charlie Hsieh <charlieofficial@gmail.com>
2023-01-05 02:19:11 +0800

    add ntp using systemd-timesyncd

--- lib/L_00_ntp.sh
+++ lib/L_00_ntp.sh
@@ -2,35 +2,53 @@ L_NTP_DATETIME() {
   #-----------------------------------------------------------------------------------------
   # NTP update date time and hwclock to prevent mariadb cause systemd warning
   #-----------------------------------------------------------------------------------------
-  echo "---------------------------------------------------"
-  echo "NTP(chrony) ---> pool.ntp.org"
-  echo "---------------------------------------------------"
-  dpkg -l chrony 2>/dev/null >/dev/null || apt install -y chrony
-  # make sure chronyd stop first , before syncing time using chronyd command!
-  # in ubuntu chronyd.service is an alias of chron.service
-  # systemctl stop chronyd
-  # systemctl disable chronyd
+  if_chrony_found="$(dpkg -l chrony 2>/dev/null | grep "ii")"
+  if_timesyncd_found="$(dpkg -l systemd-timesyncd | grep "ii")"
 
-  default_chrony_status="$(systemctl list-unit-files |grep chrony | grep service|grep enabled | head -n 1)"
+  # ------------------------------------
+  # Chrony
+  # ------------------------------------
+  if [[ -n "${if_chrony_found}" ]]; then
+    echo "---------------------------------------------------"
+    echo "NTP(chrony) ---> pool.ntp.org"
+    echo "---------------------------------------------------"
+    # make sure chronyd stop first , before syncing time using chronyd command!
+    # in ubuntu chronyd.service is an alias of chron.service
+    # systemctl stop chronyd
+    # systemctl disable chronyd
 
-  systemctl stop chrony
-  systemctl disable chrony
+    if_chrony_default_enabled="$(systemctl list-unit-files |grep chrony | grep service|grep enabled | head -n 1)"
 
-  echo "RUN: chronyd -q 'pool pool.ntp.org iburst'"
+    systemctl stop chrony
+    systemctl disable chrony
 
-  # https://chrony.tuxfamily.org/faq.html
-  # chronyd -q 'pool pool.ntp.org iburst'
-  chronyd -q -t 1 'server pool.ntp.org iburst maxsamples 1'
+    echo "RUN: chronyd -q 'pool pool.ntp.org iburst'"
 
-  echo "RUN: hwclock -w"
-  hwclock -w
+    # https://chrony.tuxfamily.org/faq.html
+    # chronyd -q 'pool pool.ntp.org iburst'
+    chronyd -q -t 1 'server pool.ntp.org iburst maxsamples 1'
+
+    # echo "RUN: hwclock -w"
+    # hwclock -w
+    if [[ -n "${if_chrony_default_enabled}" ]]; then
+      systemctl start chrony
+      systemctl enable chrony
+    fi
+
+  # ------------------------------------
+  # timesyncd
+  # ------------------------------------
+  elif [[ -n "${if_timesyncd_found}" ]]; then
+    echo "---------------------------------------------------"
+    echo "NTP(systemd-timesyncd) ---> $(cat /etc/systemd/timesyncd.conf |grep "^NTP" | cut -d'=' -f2- | awk '{print $1}')"
+    echo "---------------------------------------------------"
+    systemctl stop systemd-timesyncd
+    systemctl start systemd-timesyncd
+
+  fi
 
   echo "---------------------------------------------------"
   echo ""
 
-  if [[ -n "${default_chrony_status}" ]]; then
-    systemctl start chrony
-    systemctl enable chrony
-  fi
 
 }
